ddeaea8fa93141c9953f0127444becab63aa8fb9
Lukasz <lukasz.chludzinski@gmail.com>
2020-04-13 23:53:13 +0200

    intro tasks solved

--- common/styles.js
+++ common/styles.js
@@ -1,15 +1,55 @@
-const styles = {
-    textSize: {
-        title: 23,
-        subtitle: 18,
-        normal: 16
-    },
-    color: {
-        background: "#009",
-        title: "#f00",
-        subtitle: "#0f0",
-        normal: "#222"
+const color = {
+    background: "#009",
+    title: "#f00",
+    subtitle: "#0f0",
+    normal: "#222"
+};
+const size = {
+    title: 23,
+    subtitle: 18,
+    normal: 16
+};
+const mainView = {
+    backgroundColor: color.background
+};
+const title = {
+    color: color.title,
+    fontSize: size.title
+};
+const subtitle = {
+    color: color.subtitle,
+    fontSize: size.subtitle
+};
+const inputContainer = {
+    flexDirection: "row",
+    justifyContent: 'center',
+    backgroundColor: "green",
+    padding: 30
+};
+const addButton = {
+    backgroundColor: "grey", padding: 10, width: 100, alignItems: "center", justifyContent: "center"
+};
+const todo = {
+    marginTop: 10,
+    backgroundColor: "pink",
+    padding: 10,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignSelf: "stretch"
+};
+const removeButton = {
+    padding: 10,
+    backgroundColor: "red",
+    alignItems: "center", justifyContent: "center"
+};
 
-    }
+const styles = {
+    mainView,
+    title,
+    subtitle,
+    inputContainer,
+    todo,
+    removeButton,
+    addButton
 }
 export default styles; 
