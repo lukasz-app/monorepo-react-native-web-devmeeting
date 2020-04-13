f2f5c0740460bb6f87a591ef592a6c28e7f9c511
Lukasz <lukasz.chludzinski@gmail.com>
2020-04-13 21:12:22 +0200

    common styles

---
+++ common/styles.js
@@ -0,0 +1,15 @@
+const styles = {
+    textSize: {
+        title: 23,
+        subtitle: 18,
+        normal: 16
+    },
+    color: {
+        background: "#009",
+        title: "#f00",
+        subtitle: "#0f0",
+        normal: "#222"
+
+    }
+}
+export default styles; 
--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 import { title, subtitle } from 'common/app.json';
+import commonStyles from 'common/styles';
 
 const App = () => {
   return (
-    <View style={styles.mainContainer}>
-      <Text>{title}</Text>
-      <Text>{subtitle}</Text>
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.appContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+    backgroundColor: commonStyles.color.background,
+  },
+  appContainer: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    color: commonStyles.color.title,
+    fontSize: commonStyles.textSize.title,
+  },
+  subtitle: {
+    marginTop: 10,
+    color: commonStyles.color.subtitle,
+    fontSize: commonStyles.textSize.subtitle,
   },
 });
 
