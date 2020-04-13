0a1a42c3aae6dc7bf743205402952fba70a09fa7
Lukasz <lukasz.chludzinski@gmail.com>
2020-04-13 21:12:22 +0200

    common styles

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { title, subtitle } from 'common/app.json';
+import commonStyles from 'common/styles';
 
 function App() {
   return (
-    <div className="App">
-      <p>
+    <div className="App" style={{ flex: 1, backgroundColor: commonStyles.color.background }}  >
+      <p style={{ color: commonStyles.color.title, fontSize: commonStyles.textSize.title }} >
         {title}
       </p>
-      <p>
+      <p style={{ color: commonStyles.color.subtitle, fontSize: commonStyles.textSize.subtitle }} >
         {subtitle}
       </p>
     </div>
