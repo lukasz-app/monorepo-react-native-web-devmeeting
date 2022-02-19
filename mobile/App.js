6219bd9878e07cc4df237612d3d70e798422496a
Lukasz Chludzinski <lukasz.chludzinski@gmail.com>
2022-02-19 22:01:23 +0100

    version bump: react native 0.67.2, react 17.0.2

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { title, subtitle } from 'common/app.json';
+import {StyleSheet, View, Text} from 'react-native';
+import {title, subtitle} from 'common/app.json';
 
 const App = () => {
   return (
