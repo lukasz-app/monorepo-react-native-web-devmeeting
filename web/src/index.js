8eef27ab57d2b0dfc603e82e6ec49a1e9aef2300
Lukasz Chludzinski <lukasz.chludzinski@gmail.com>
2022-02-19 22:46:23 +0100

    version bump: react native 0.67.2, react 17.0.2

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,11 @@
-import Router from 'common/navigation/Router';
-import * as serviceWorker from './serviceWorker';
-import { AppRegistry } from 'react-native'
+import Router from "common/navigation/Router";
+import * as serviceWorker from "./serviceWorker";
+import { AppRegistry } from "react-native";
 import { createBrowserApp } from "@react-navigation/web";
 
-AppRegistry.registerComponent('myprojectname', () => createBrowserApp(Router))
-AppRegistry.runApplication('myprojectname', {
-  rootTag: document.getElementById('root'),
-})
+AppRegistry.registerComponent("myprojectname", () => createBrowserApp(Router));
+AppRegistry.runApplication("myprojectname", {
+  rootTag: document.getElementById("root"),
+});
 
 serviceWorker.unregister();
